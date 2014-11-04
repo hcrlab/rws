ae167c9a9c361e33f7b1917090cbf1cd2db72ff6
Justin Huang <jstn@cs.washington.edu>
2014-11-04 14:13:56 -0800

    Add comments to UserVerifier.

--- src/users.py
+++ src/users.py
@@ -6,11 +6,18 @@ class UserVerifierError(Enum):
   DISALLOWED_USER = 3
 
 class UserVerifier(object):
+  """Verifies user login using Google Identity Toolkit."""
   def __init__(self, gitkit_client, allowed_users):
     self._gitkit_client = gitkit_client
     self._allowed_users = allowed_users
 
   def check_user(self, request):
+    """Checks the user login cookie.
+
+    Returns: (email, error), Email is the string email address of the person who
+      logged in, or None if the login was invalid. Error is a UserVerifierError
+      describing why the login was invalid, or None if the login was valid.
+    """
     if 'gtoken' not in request.cookies:
       return None, UserVerifierError.NO_COOKIE
 
