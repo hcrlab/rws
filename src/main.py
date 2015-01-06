560212ee5e133d67e7889c4ae92fbb351d80d2f4
Justin Huang <jstn@cs.washington.edu>
2015-01-05 17:55:55 -0800

    Fix command line args, can now roslaunch rws rws.launch.

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ from identitytoolkit import gitkitclient
 import apps
 import argparse
 import os
+import rospy
 import secrets
 import sys
 import threading
@@ -102,6 +103,7 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='Robot web server.')
   parser.add_argument('debug', type=bool, default=False,
       help='Whether to start the server in debug mode.')
+  sys.argv = rospy.myargv()
   args = parser.parse_args()
 
   app.run(host='0.0.0.0', debug=args.debug)
