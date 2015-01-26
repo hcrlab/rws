f24e3d10069ce25f16871ce1f8c3d4d6c390fadc
Justin Huang <jstn@cs.washington.edu>
2015-01-25 18:16:14 -0800

    Launch websocket server automatically on startup.

---
+++ src/websocket.py
@@ -0,0 +1,35 @@
+import os
+import subprocess
+
+# TODO(jstn): If there are enough classes like these that duplicate roslaunch
+# functionality (see App), then make a separate Launcher class.
+class WebsocketServer(object):
+    """Websocket server launcher.
+    
+    Example usage:
+    ws = WebsocketServer(9999)
+    ws.launch()
+    ...
+    ws.is_running()
+    ...
+    ws.terminate()
+    """
+    def __init__(self, port):
+        self._port = port
+        self._subprocess = None
+
+    def is_running(self):
+        return self._subprocess is not None
+
+    def launch(self):
+        if self._subprocess is None:
+            self._subprocess = subprocess.Popen(
+                ['roslaunch', 'rosbridge_server', 'rosbridge_websocket.launch',
+                'port:={}'.format(self._port)],
+                env=os.environ
+            )
+
+    def terminate(self):
+        self._subprocess.terminate()
+        self._subprocess.wait()
+        self._subprocess = None
