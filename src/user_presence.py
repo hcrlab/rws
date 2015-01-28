6985e2dbda9c6be2dc1f58e4a3034bbfb574194b
Christopher Su <christophersu9@gmail.com>
2015-01-27 18:45:18 -0800

    progress on api

--- src/user_presence.py
+++ src/user_presence.py
@@ -1,7 +1,20 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from sets import Set
 
 blueprint = Blueprint('user_presence', __name__)
 
-@blueprint.route('/remove')
+current_users = Set()
+
+@blueprint.route('/current', methods=['GET'])
+def get_current_users:
+    return jsonify(current_users)
+
+@blueprint.route('/add', methods=['POST'])
+def add_user():
+    user = request.form['user']
+    current_users.add(user)
+
+@blueprint.route('/remove', methods=['POST'])
 def remove_user():
-    user_to_remove = request.form['user']
+    user = request.form['user']
+    current_users.pop(user)
