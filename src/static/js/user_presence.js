9d511322985200e4004a1cce2bcaa0424d8baf58
Christopher Su <christophersu9@gmail.com>
2015-02-04 12:25:12 -0800

    progress

--- src/static/js/user_presence.js
+++ src/static/js/user_presence.js
@@ -1,13 +1,15 @@
 window.onload = function(e) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", "/api/user_presence/add", false );
-    xmlHttp.send( "user={{ user_email }}" );
+    xmlHttp.open("POST", "/api/user_presence/add", false);
+    console.log("adding: " + "user=" + user + "&location=" + location)
+    xmlHttp.send("user=" + user + "&location=" + location);
     return xmlHttp.responseText;
 };
 
 window.onbeforeunload = function(e) {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", "/api/user_presence/remove", false );
-    xmlHttp.send( "user={{ user_email }}" );
+    xmlHttp.open("POST", "/api/user_presence/remove", false );
+    console.log("removing: " + "user=" + user + "&location=" + location)
+    xmlHttp.send("user=" + user + "&location=" + location);
     return xmlHttp.responseText;
 };
--- src/user_presence.py
+++ src/user_presence.py
@@ -3,20 +3,26 @@ from sets import Set
 
 blueprint = Blueprint('user_presence', __name__)
 
-current_users = Set()
+current_users = dict()
+
+def add_user(location, user):
+    if location not in current_users:
+        current_users[location] = Set()
+    current_users.add(user)
 
 @blueprint.route('/current', methods=['GET'])
-def get_current_users:
+def get_users_endpoint():
     return jsonify(current_users)
 
 @blueprint.route('/add', methods=['POST'])
-def add_user():
-    user = request.form['user']
+def add_user_endpoint():
+    add_user(request.form['location'] ,request.form['user'])
     current_users.add(user)
     return jsonify(current_users)
 
 @blueprint.route('/remove', methods=['POST'])
-def remove_user():
+def remove_user_endpoint():
     user = request.form['user']
-    current_users.pop(user)
+    location = request.form['location']
+    current_users[location].pop(user)
     return jsonify(current_users)
