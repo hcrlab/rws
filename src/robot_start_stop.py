7bd27131e543dc4d845647c28b6ed9424a14789c
Christopher Su <christophersu9@gmail.com>
2015-02-20 18:10:24 -0800

    corrected robot claim check

--- src/robot_start_stop.py
+++ src/robot_start_stop.py
@@ -13,12 +13,12 @@ def claim_and_start_robot():
     try:
         subprocess.check_call('robot claim -f --username ' + user + ' --email ' + user + ' -m "teleoperating the robot (claimed via Robot Web Server)"', shell=True)
     except CalledProcessError:
-        return jsonify({'status': 'error', 'message': 'failed to claim robot'})
+        return jsonify({'status': 'error', 'message': 'Failed to claim robot.'})
 
     try:
         subprocess.check_call('robot start -f', shell=True)
     except CalledProcessError:
-        return jsonify({'status': 'error', 'message': 'failed to start robot'})
+        return jsonify({'status': 'error', 'message': 'Failed to start robot.'})
 
     return jsonify({'status': 'success'})
 
@@ -27,27 +27,33 @@ def stop_robot():
     try:
         subprocess.check_call('robot stop -f', shell=True)
     except:
-        return jsonify({'status': 'error', 'message': 'failed to stop robot'})
+        return jsonify({'status': 'error', 'message': 'Failed to stop robot.'})
 
     return jsonify({'status': 'success'})
 
 @blueprint.route('/check', methods=['GET'])
 def check_robot_claim():
     active_user_file = '/var/lib/robot/active_user.yaml'
-    if os.path.exists(active_user_file):
-        fp = open(active_user_file, "r")
-        content = fp.read()
-        fp.close()
 
-        data = yaml.load(content)
-        
-        return jsonify({
-            'status': 'success',
-            'claim': data
-        })
+    # if the active_user.yaml file exists, attempt to check claim
+    if os.path.exists(active_user_file):
+        try:
+            fp = open(active_user_file, "r")
+            content = fp.read()
+            fp.close()
+            data = yaml.load(content)
+            
+            return jsonify({
+                'status': 'success',
+                'claim': data
+            })
+        except:
+            return jsonify({'status': 'error', 'message': 'Failed to check robot claim.'})
+
+    # if the file doesn't exist, then the robot is not claimed
     else:
         return jsonify({
-            'status': 'error',
-            'message': 'active user file not found',
+            'status': 'success',
+            'message': 'Robot is not claimed currently.',
             'claim': None
         })
