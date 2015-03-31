66cdc35e205cc4eede40797b85620634b7f4554a
Justin Huang <jstn@cs.washington.edu>
2015-03-30 19:42:35 -0700

    Add app launch/terminate tests.

--- src/robot_web_server_test.py
+++ src/robot_web_server_test.py
@@ -1,3 +1,4 @@
+import apps
 import mock
 import server_factory
 import unittest
@@ -7,6 +8,13 @@ import users
 class TestRobotWebServer(unittest.TestCase):
     def setUp(self):
         self._server = server_factory.test()
+
+    def mock_login(self):
+        """Mocks the UserVerifier so that the login check passes."""
+        user = type('User', (), {'email': 'test@email.com', 'name': None})()
+        self._server._user_verifier.check_user = mock.Mock(
+            return_value=(user, None))
+        return user
 
     def test_run(self):
         """Check that the Flask app and the Websocket server are run."""
@@ -24,21 +32,44 @@ class TestRobotWebServer(unittest.TestCase):
             return_value=(None, users.UserVerifierError.NO_COOKIE))
         rv = self._server._app.get('/')
         self.assertTrue('Apps' not in rv.get_data())
-        user = type('User', (), {'email': 'test@email.com', 'name': None})()
-        self._server._user_verifier.check_user = mock.Mock(
-            return_value=(user, None))
+        user = self.mock_login()
         rv = self._server._app.get('/')
         self.assertTrue(user.email in rv.get_data())
 
     def test_invalid_app(self):
         """Check that a 404 is returned for invalid app names."""
-        user = type('User', (), {'email': 'test@email.com', 'name': None})()
-        self._server._user_verifier.check_user = mock.Mock(
-            return_value=(user, None))
+        self.mock_login()
         rv = self._server._app.get('/app/testapp')
         self.assertEqual(404, rv.status_code)
         rv = self._server._app.get('/app/close/testapp')
         self.assertEqual(404, rv.status_code)
+
+    @mock.patch('apps.App')
+    def test_app_launch(self, App):
+        """Check that apps are launched in the common case."""
+        self.mock_login()
+        test_app = App('/path/to/test_app')
+        self._server._rws_apps = {'test_app': test_app}
+        rv = self._server._app.get('/app/test_app')
+        self.assertEqual(200, rv.status_code)
+        test_app.launch.assert_called_with()
+
+    @mock.patch('apps.App')
+    def test_app_close(self, App):
+        """Check that apps are terminated iff they are running."""
+        self.mock_login()
+        test_app = App('/path/to/test_app')
+        self._server._rws_apps = {'test_app': test_app}
+
+        # If app isn't running, don't call terminate()
+        test_app.is_running = mock.Mock(return_value=False)
+        rv = self._server._app.get('/app/close/test_app')
+        self.assertFalse(test_app.terminate.called)
+
+        # Otherwise, call terminate()
+        test_app.is_running = mock.Mock(return_value=True)
+        rv = self._server._app.get('/app/close/test_app')
+        self.assertTrue(test_app.terminate.called)
 
 
 if __name__ == '__main__':
