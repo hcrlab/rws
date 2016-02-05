7dd24d50b044d3c785b2a1e788fd3e67c8f5c2cb
Justin Huang <jstn@cs.washington.edu>
2016-02-04 18:44:06 -0800

    Update example secrets.

--- src/example_secrets.py
+++ src/example_secrets.py
@@ -1,14 +1,14 @@
-# The protocol, server, and port we are serving this site under.
-SERVER_ORIGIN = 'localhost:5000'
-HOST_NAME = 'localhost'
+# The Google Client ID for your application, used for Google Sign-in.
+# See https://developers.google.com/identity/sign-in/web/devconsole-project
+GOOGLE_CLIENT_ID = '1234-abcd.apps.googleusercontent.com'
 
-# Not really a secret, but it's probably better to leave it out of source
-# control. API key for the OAuth 2 callback.
-BROWSER_API_KEY = 'AIza...'
+# A launch file to bring up the robot with.
+BRINGUP_FILE = '/home/rosie/bringup.launch'
 
-GITKIT_SERVER_CONFIG_PATH = '../resources/gitkit/gitkit-server-config.json'
-
-# List of users allowed to log in.
-ALLOWED_USERS = ['example@example.com', ]
-
+# The catkin workspace to load apps from.
 CATKIN_WS = '/home/rosie/rws/catkin_ws'
+
+# When developing, we use "gulp serve" to serve the frontend from localhost:5000.
+# If you are serving the frontend on a different host/port, change it here.
+# This is used to enable cross-origin resource sharing in development.
+DEV_FRONTEND_ORIGIN = 'http://localhost:5000'
